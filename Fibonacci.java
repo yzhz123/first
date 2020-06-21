a3b6ec0d1a4c8d456816155f2da4f701bd63e92e
yzhz123 <67141484+yzhz123@users.noreply.github.com>
2020-06-21 22:44:13 +0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,13 +4,13 @@ public class Fibonacci {
 	
 	public static int fibonacci(int n){
 
-        if (n == 1 || n == 2) {             //特殊情况，分开讨论
+        if (n == 1 || n == 2) {            
             return 1;
         }
         if (n > 2) {
-            return fibonacci(n - 1) + fibonacci(n - 2);     //递归调用
+            return fibonacci(n - 1) + fibonacci(n - 2);     
         }
-        return -1;              //如果输入错误的n，一律返回-1
+        return -2;             
     }
 
 	public static void main(String[] args) {
